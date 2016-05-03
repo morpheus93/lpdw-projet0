a34ee3f5272d81063c7f740f1546b0103194f86c
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-05-03 11:43:46 +0200

    Added some dependencyes / Bootstrap

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,29 @@
-dd
+var app = angular.module('projet2', [
+'ui.router',
+'ui.bootstrap'
+    ]);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /
+  $urlRouterProvider.otherwise("/");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('main', {
+      url: "/",
+      templateUrl: "views/index.html",
+      reload:true
+    });
+});
+
+app.controller('mainCtrl', ['$scope', '$http','$rootScope','$location', function($scope, $http,$rootScope,$location)
+{
+  $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
+    // called when a state change
+  });
+  $rootScope.$on('$stateChangeSuccess', function (event) {
+    // called when a state has changed
+  });
+
+}]);

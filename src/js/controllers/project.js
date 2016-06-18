305ef0e5f59be18a41e800df34bb46eb34445962
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-06-18 18:39:13 +0200

    Should retreive projects correctly

--- src/js/controllers/project.js
+++ src/js/controllers/project.js
@@ -1,13 +1,19 @@
 app.controller('projectCtrl', function($scope,$state,$rootScope,$http)
 {
+
+  var apiUri = $rootScope.apiAddress+'/projects'+'?access_token='+$rootScope.access_token;
+  if($state.params.id){
+    apiUri = $rootScope.apiAddress+'/projects/'+$state.params.id+'?access_token='+$rootScope.access_token;;
+  }
+
   $http({
-  method: 'GET',
-  url: $rootScope.apiAddress+'/projects'
-}).then(function successCallback(response) {
-  console.log(response);
-  $scope.project = response;
-  }, function errorCallback(response) {
+    method: 'GET',
+    url: apiUri
+  }).then(function successCallback(response) {
     console.log(response);
-    $state.go('main');
-  });
+    $scope.projects = response.data;
+    }, function errorCallback(response) {
+      console.log(response);
+      $state.go('main');
+    });
 });

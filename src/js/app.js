6ae293e163854f056b2447c0b6182215294327be
ombrette <lamberthema@gmail.com>
2016-06-19 21:56:02 +0200

    global current user + add route org

--- src/js/app.js
+++ src/js/app.js
@@ -36,13 +36,18 @@ app.config(function($stateProvider, $urlRouterProvider) {
       templateUrl: "views/login.html",
       controller : "loginCtrl"
     })
-    .state('organization', {
-      url: "/organizations/{id:int}",
-      templateUrl: "views/organizations.html",
+    .state('organizations', {
+      url: "/organizations",
+      templateUrl: "views/organizations/list.html",
       controller : "organizationCtrl"
     })
-    .state('account', {
-      url: "/account/{id:int}",
+    .state('organizations.item', {
+      url: "/organizations/{id:int}",
+      templateUrl: "views/organizations/organization.html",
+      controller : "organizationCtrl"
+    })
+    .state('users', {
+      url: "/users",
       templateUrl: "views/users.html",
       controller : "accountCtrl"
     })
@@ -65,10 +70,10 @@ app.config(function($stateProvider, $urlRouterProvider) {
       url: "/{id:int}",
       templateUrl: "views/projects/project.html",
     })
-    .state('projects.new', {
+    /*.state('projects.new', {
       url: "/new",
       templateUrl: "views/projects/new.html",
-    })
+    })*/
     /*.state('project', {
       url: "/project",
       templateUrl: "views/projects/project.html",
@@ -98,10 +103,12 @@ app.config(function (localStorageServiceProvider) {
     .setPrefix('colab');
 });
 
-app.controller('mainCtrl', ['$scope', '$http','$rootScope','$location','localStorageService', function($scope, $http,$rootScope,$location,localStorageService)
+app.controller('mainCtrl', ['$scope', '$http','$rootScope','$location','$state','localStorageService', function($scope, $http,$rootScope,$location,$state,localStorageService)
 {
   var apiAddress = "http://"+$location.host()+":"+$location.port()+"/api";
+  
   $rootScope.apiAddress = apiAddress;
+  
   $rootScope.access_token = "";
   $rootScope.logged = false;
 
@@ -130,6 +137,19 @@ app.controller('mainCtrl', ['$scope', '$http','$rootScope','$location','localSto
       location.reload();
     }
   }
+
+  var apiUri = $rootScope.apiAddress+'/me'+'?access_token='+$rootScope.access_token;
+  
+  $http({
+    method: 'GET',
+    url: apiUri
+  }).then(function successCallback(response) {
+    console.log(response);
+    $rootScope.me = response.data;
+    }, function errorCallback(response) {
+      console.log(response);
+      $state.go('endingSignup');
+    });
 
 
   $(".menu-btn").click(function(){
--- src/js/controllers/account.js
+++ src/js/controllers/account.js
@@ -1,18 +1,5 @@
 app.controller('accountCtrl', function($scope,$state,$rootScope,$http)
 {
-  var apiUri = $rootScope.apiAddress+'/me'+'?access_token='+$rootScope.access_token;
-
-  $http({
-    method: 'GET',
-    url: apiUri
-  }).then(function successCallback(response) {
-    console.log(response);
-    $scope.me = response.data;
-    }, function errorCallback(response) {
-      console.log(response);
-      $state.go('endingSignup');
-    });
-
     $scope.profilInfos = {};
     $scope.processing = false;
     $scope.success = false;

6c929815e9f64afb9b4e928d6b33dadd7015fa6b
ombrette <lamberthema@gmail.com>
2016-06-20 02:37:41 +0200

    dyn promise  + anct list + org list + projects list

--- src/js/controllers/organization.js
+++ src/js/controllers/organization.js
@@ -1,17 +1,14 @@
 app.controller('organizationCtrl', function($scope,$state,$rootScope,$http)
 {
 
-  var apiUri = $rootScope.apiAddress+'/projects'+'?access_token='+$rootScope.access_token;
-  if($state.params.id){
-    apiUri = $rootScope.apiAddress+'/projects/'+$state.params.id+'?access_token='+$rootScope.access_token;;
-  }
+  var apiUri = $rootScope.apiAddress+'/associations'+'?access_token='+$rootScope.access_token;
 
   $http({
     method: 'GET',
     url: apiUri
   }).then(function successCallback(response) {
     console.log(response);
-    $scope.projects = response.data;
+    $scope.organizations = response.data;
     }, function errorCallback(response) {
       console.log(response);
       $state.go('login');
